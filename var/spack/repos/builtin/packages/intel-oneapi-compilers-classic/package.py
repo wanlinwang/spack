6974a1ec3788dc55775e51938092acacc73b06bc
Gregory Becker <becker33@llnl.gov>
2022-06-24 16:38:15 -0700

    intel-oneapi-compilers-classic: symlinks to underlying compiler

--- var/spack/repos/builtin/packages/intel-oneapi-compilers-classic/package.py
+++ var/spack/repos/builtin/packages/intel-oneapi-compilers-classic/package.py
@@ -21,15 +21,20 @@ class IntelOneapiCompilersClassic(Package):
 
     phases = []
 
-    for ver in ['2022.1.0',
-                '2022.0.2',
-                '2022.0.1',
-                '2021.4.0',
-                '2021.3.0',
-                '2021.2.0',
-                '2021.1.2']:
+    # Versions before 2021 are in the `intel` package
+    # intel-oneapi versions before 2022 use intel@19.0.4
+    for ver, oneapi_ver in {
+        '2021.6.0': '2022.2.0',
+        '2021.5.0': '2022.1.0',
+    }.items():
         version(ver)
-        depends_on('intel-oneapi-compilers@' + ver, when='@' + ver, type='run')
+        depends_on('intel-oneapi-compilers@' + oneapi_ver, when='@' + ver, type='run')
+
+    @property
+    def oneapi_compiler_prefix(self):
+        oneapi_version = self.spec['intel-oneapi-compilers'].version
+        return self.spec['intel-oneapi-compilers'].prefix.compiler.join(
+            '%s' % oneapi_version).linux
 
     def setup_run_environment(self, env):
         """Adds environment variables to the generated module file.
@@ -42,9 +47,22 @@ class IntelOneapiCompilersClassic(Package):
 
         and from setting CC/CXX/F77/FC
         """
-        bin = join_path(self.spec['intel-oneapi-compilers'].prefix,
-                        'compile', 'linux', 'bin', 'intel64')
-        env.set('CC', join_path(bin, 'icc'))
-        env.set('CXX', join_path(bin, 'icpc'))
-        env.set('F77', join_path(bin, 'ifort'))
-        env.set('FC', join_path(bin, 'ifort'))
+        env.set('CC', join_path(self.oneapi_compiler_prefix.bin, 'icc'))
+        env.set('CXX', join_path(self.oneapi_compiler_prefix.bin, 'icpc'))
+        env.set('F77', join_path(self.oneapi_compiler_prefix.bin, 'ifort'))
+        env.set('FC', join_path(self.oneapi_compiler_prefix.bin, 'ifort'))
+
+    def install(self, spec, prefix):
+        # We create a physical directory for binaries because they are more likely
+        # to be affected by being symlinked
+        mkdirp(prefix.bin)
+        for entry in os.listdir(self.oneapi_compiler_prefix.bin):
+            src = os.path.join(self.oneapi_compiler_prefix.bin, entry)
+            dst = prefix.bin.join(entry)
+            os.symlink(src, dst)
+
+        os.symlink(self.oneapi_compiler_prefix.lib, prefix.lib)
+        os.symlink(self.oneapi_compiler_prefix.include, prefix.include)
+        os.symlink(self.oneapi_compiler_prefix.compiler, prefix.compiler)
+        os.symlink(self.oneapi_compiler_prefix.man, prefix.man)
+        os.symlink(self.oneapi_compiler_prefix.share, prefix.share)
